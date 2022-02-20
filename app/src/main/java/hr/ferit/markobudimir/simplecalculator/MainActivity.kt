8bc619ee988708c7ae6bbd09a1b3f55826b6505a
Marko Budimir <marko.budimir12@gmail.com>
2022-02-20 22:05:47 +0100

    Add clear, decimal point and onDigit functionality

--- app/src/main/java/hr/ferit/markobudimir/simplecalculator/MainActivity.kt
+++ app/src/main/java/hr/ferit/markobudimir/simplecalculator/MainActivity.kt
@@ -3,15 +3,72 @@ package hr.ferit.markobudimir.simplecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvResult: TextView? = null
+    private var btnClear : Button? = null
+    private var btnAdd : Button? = null
+    private var btnSubtract : Button? = null
+    private var btnMultiply: Button? = null
+    private var btnDivide: Button? = null
+    private var btnEqual: Button? = null
+    private var btnDecimalPoint: Button? = null
+    private var lastNumeric : Boolean = false
+    private var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvResult = findViewById(R.id.tvResult)
+        btnClear = findViewById(R.id.btnClear)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSubtract = findViewById(R.id.btnSubtract)
+        btnMultiply = findViewById(R.id.btnMultiply)
+        btnDivide = findViewById(R.id.btnDivide)
+        btnEqual = findViewById(R.id.btnEqual)
+        btnDecimalPoint = findViewById(R.id.btnDot)
+
+        btnClear?.setOnClickListener {
+            tvResult?.text = ""
+            lastNumeric = false
+            lastDot = false
+        }
+
+        btnAdd?.setOnClickListener {
+
+        }
+
+        btnSubtract?.setOnClickListener {
+
+        }
+
+        btnMultiply?.setOnClickListener {
+
+        }
+
+        btnDivide?.setOnClickListener {
+
+        }
+
+        btnDecimalPoint?.setOnClickListener {
+            if(lastNumeric && !lastDot){
+                tvResult?.append(".")
+                lastNumeric = false
+                lastDot = true
+            }
+        }
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
+        tvResult?.append((view as Button).text)
+        lastNumeric = true
     }
+
+
+
+
 }
